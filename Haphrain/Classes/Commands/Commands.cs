6575e89ae6985a3dad67345050f15c761b4aabfa
Cruciatum <spc.snake@hotmail.com>
2019-05-23 11:15:06 +0200

    Added command for help

---
+++ Haphrain/Classes/Commands/Commands.cs
@@ -0,0 +1,21 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Haphrain.Classes.Commands
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("commands"), Alias("cmds"), Summary("View a list of available commands")]
+        public async Task Cmds()
+        {
+            var builder = new EmbedBuilder();
+            builder.Title = "Commands";
+            builder.AddField("Coinflip (cf,coin,flip)", "Flip a coin to get heads or tails");
+            builder.AddField("SetPrefix (Prefix, NewPrefix)", "Select a new prefix to be used on this server (Administrators only)");
+            builder.AddField("Goodbye", "Make the bot leave the server (Owner only)");
+
+            await Context.Channel.SendMessageAsync(null, false, builder.Build());
+        }
+    }
+}
