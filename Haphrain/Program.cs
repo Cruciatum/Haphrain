a0fbd0728726797f2895f27994b187197a107e48
Cruciatum <spc.snake@hotmail.com>
2019-05-23 10:25:53 +0200

    Initial

---
+++ Haphrain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Haphrain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
