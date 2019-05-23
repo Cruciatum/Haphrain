70b0467b20d5ff1fabd71b01833577183dbe632e
Cruciatum <spc.snake@hotmail.com>
2019-05-23 10:55:15 +0200

    More starting stuff

---
+++ Haphrain/Classes/Commands/TestCommands.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Haphrain.Classes.Commands
+{
+    public class ModCommands : ModuleBase<SocketCommandContext>
+    {
+        [Command("coinflip"), Alias("cf", "coin", "flip"), Summary("Flip a coin")]
+        public async Task coinflip()
+        {
+            int result = 0;
+            Random r = new Random();
+            result = r.Next(0, 100);
+
+            Console.WriteLine($"{DateTime.Now} -> Executed coin flip, result: {result}");
+            await Context.Channel.SendMessageAsync($"{Context.User.Mention} your coin landed on {(result % 2 == 0 ? "Heads" : "Tails")}");
+        }
+
+        [Command("setprefix"), Alias("prefix", "newprefix"), Summary("Set a new prefix for this server"), RequireUserPermission(GuildPermission.Administrator)]
+        public async Task setPrefix(string newPrefix)
+        {
+            if (newPrefix != null && newPrefix != "")
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Guilds.xml"));
+
+                var guildNode = doc.SelectSingleNode($"/Guilds/Guild[@GuildID='{Context.Guild.Id}']");
+                var prefixNode = guildNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "Prefix");
+                prefixNode.InnerText = newPrefix;
+
+                doc.Save(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Guilds.xml"));
+
+                await Context.Channel.SendMessageAsync($"{Context.User.Mention}, I have updated your server's prefix to {newPrefix}");
+            }
+        }
+
+        [Command("goodbye"), Summary("Leave a server"), RequireOwner]
+        public async Task leaveGuild()
+        {
+            await Context.Channel.SendMessageAsync($"Goodbye {Context.Guild.Owner.Mention}, apparantly {Context.User.Mention} wants me gone :sob:");
+            await Context.Guild.LeaveAsync();
+        }
+    }
+}
--- Haphrain/Program.cs
+++ Haphrain/Program.cs
@@ -1,12 +1,148 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Haphrain
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+        private XmlDocument GuildsFile = new XmlDocument();
+        private readonly string GuildsFileLoc = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Guilds.xml");
+
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+            Client.JoinedGuild += Client_JoinedGuild;
+            Client.LeftGuild += Client_LeftGuild;
+
+            string Token = "";
+            using (var s = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Token.txt"), FileMode.Open, FileAccess.Read))
+            {
+                using (var r = new StreamReader(s))
+                {
+                    Token = r.ReadToEnd();
+                }
+            }
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_LeftGuild(SocketGuild arg)
+        {
+            Console.WriteLine($"{DateTime.Now} -> Left guild: {arg.Id}");
+
+            GuildsFile.Load(GuildsFileLoc);
+            //remove guildobject from Guilds file
+            var root = GuildsFile.DocumentElement;
+            var guildNode = GuildsFile.SelectSingleNode($"/Guilds/Guild[@GuildID='{arg.Id}']");
+
+            root.RemoveChild(guildNode);
+
+            GuildsFile.Save(GuildsFileLoc);
+
+            await Task.Delay(100);
+
+        }
+
+        private async Task Client_JoinedGuild(SocketGuild arg)
+        {
+            Console.WriteLine($"{DateTime.Now} -> Joined guild: {arg.Id}");
+
+            GuildsFile.Load(GuildsFileLoc);
+            //add new guildobject to Guilds file
+            var root = GuildsFile.DocumentElement;
+
+            var guildNode = GuildsFile.CreateElement("Guild");
+            var guildID = GuildsFile.CreateAttribute("GuildID");
+            var prefixNode = GuildsFile.CreateElement("Prefix");
+            var nameNode = GuildsFile.CreateElement("GuildName");
+            var ownerID = GuildsFile.CreateElement("OwnerID");
+
+            guildID.Value = arg.Id.ToString();
+            guildNode.Attributes.Append(guildID);
+
+            nameNode.InnerText = arg.Name;
+            guildNode.AppendChild(nameNode);
+
+            ownerID.InnerText = arg.Owner.Id.ToString();
+            guildNode.AppendChild(ownerID);
+            
+            prefixNode.InnerText = "]";
+            guildNode.AppendChild(prefixNode);
+
+            root.AppendChild(guildNode);
+
+            GuildsFile.Save(GuildsFileLoc);
+
+            await Task.Delay(100);
+        }
+
+        private async Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine($"{DateTime.Now} at {arg.Source} -> {arg.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Developing this badboy");
+        }
+
+        private async Task Client_MessageReceived(SocketMessage arg)
+        {
+            var msg = arg as SocketUserMessage;
+            var context = new SocketCommandContext(Client, msg);
+
+            if (context.Message == null || context.Message.Content == "") return;
+            if (context.User.IsBot) return;
+
+            int argPos = 0;
+            string guildPrefix = "]";
+
+            GuildsFile.Load(GuildsFileLoc);
+            var guildNode = GuildsFile.SelectSingleNode($"/Guilds/Guild[@GuildID='{context.Guild.Id}']");
+            var prefixNode = guildNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "Prefix");
+
+            if (prefixNode != null) guildPrefix = prefixNode.InnerText;
+
+            if (!(msg.HasStringPrefix(guildPrefix, ref argPos))) return;
+
+            var Result = await Commands.ExecuteAsync(context, argPos, null);
+            if (!Result.IsSuccess)
+            {
+                Console.WriteLine($"{DateTime.Now} at Commands -> Something went wrong when executing a command.");
+                Console.WriteLine($"Command text: {context.Message.Content} | Error: {Result.ErrorReason}");
+            }
         }
     }
 }
