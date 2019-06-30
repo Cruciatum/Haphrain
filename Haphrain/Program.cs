6e130d634c8820756ee93c5f43376a546b1b068d
Cruciatum <yorrick.dekempe@gmail.com>
2019-06-30 16:31:45 +0200

    Added embed and attachment loggins as well as guild-specific options

--- Haphrain/Program.cs
+++ Haphrain/Program.cs
@@ -11,6 +11,8 @@ using Discord.Commands;
 using System.Diagnostics;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
+using Haphrain.Classes.HelperObjects;
+using System.Net;
 
 namespace Haphrain
 {
@@ -90,6 +92,7 @@ namespace Haphrain
 
             GuildsFile.Save(GuildsFileLoc);
 
+            await UpdateActivity();
             await Task.Delay(100);
 
         }
@@ -107,6 +110,11 @@ namespace Haphrain
             var prefixNode = GuildsFile.CreateElement("Prefix");
             var nameNode = GuildsFile.CreateElement("GuildName");
             var ownerID = GuildsFile.CreateElement("OwnerID");
+            var optionsNode = GuildsFile.CreateElement("Options");
+            var optionLogEmbed = optionsNode.AppendChild(GuildsFile.CreateElement("LogEmbeds")).InnerText = "0";
+            var optionLogAttachments = optionsNode.AppendChild(GuildsFile.CreateElement("LogAttachments")).InnerText = "0";
+            
+            var optionLogChannelID = optionsNode.AppendChild(GuildsFile.CreateElement("LogChannelID")).InnerText = "0";
 
             guildID.Value = arg.Id.ToString();
             guildNode.Attributes.Append(guildID);
@@ -120,10 +128,13 @@ namespace Haphrain
             prefixNode.InnerText = "]";
             guildNode.AppendChild(prefixNode);
 
+            guildNode.AppendChild(optionsNode);
+
             root.AppendChild(guildNode);
 
             GuildsFile.Save(GuildsFileLoc);
 
+            await UpdateActivity();
             await Task.Delay(100);
         }
 
@@ -134,16 +145,19 @@ namespace Haphrain
 
         private async Task Client_Ready()
         {
-            await Client.SetGameAsync("Developing this badboy");
+            await UpdateActivity();
             await CheckGuildsStartup();
+            Console.WriteLine("Bot ready!");
         }
 
         private async Task Client_MessageReceived(SocketMessage arg)
         {
             var msg = arg as SocketUserMessage;
+            var guildOptions = new Options();
+
             var context = new SocketCommandContext(Client, msg);
 
-            if (context.Message == null || context.Message.Content == "") return;
+            if ((context.Message == null || context.Message.Content == "") && arg.Attachments.Count == 0 && arg.Embeds.Count==0) return;
             if (context.User.IsBot) return;
 
             int argPos = 0;
@@ -152,6 +166,18 @@ namespace Haphrain
             GuildsFile.Load(GuildsFileLoc);
             var guildNode = GuildsFile.SelectSingleNode($"/Guilds/Guild[@GuildID='{context.Guild.Id}']");
             var prefixNode = guildNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "Prefix");
+            var optionsNode = guildNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "Options");
+            guildOptions.LogEmbeds = (optionsNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "LogEmbeds").InnerText == "0") ? false : true;
+            guildOptions.LogAttachments = (optionsNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "LogAttachments").InnerText == "0") ? false : true;
+            guildOptions.LogChannelID = ulong.Parse(optionsNode.ChildNodes.Cast<XmlNode>().SingleOrDefault(n => n.Name == "LogChannelID").InnerText);
+
+            if (guildOptions.LogChannelID != 0)
+            {
+                if (guildOptions.LogEmbeds)
+                    if (msg.Embeds.Count > 0) { await LogEmbed(msg, guildOptions.LogChannelID, Client); }
+                if (guildOptions.LogAttachments)
+                    if (msg.Attachments.Count > 0) { await LogAttachment(msg, guildOptions.LogChannelID, Client); }
+            }
 
             if (prefixNode != null) guildPrefix = prefixNode.InnerText;
 
@@ -161,7 +187,78 @@ namespace Haphrain
             if (!Result.IsSuccess)
             {
                 Console.WriteLine($"{DateTime.Now} at Commands -> Something went wrong when executing a command.");
-                Console.WriteLine($"Command text: {context.Message.Content} | Error: {Result.ErrorReason}");
+                Console.WriteLine($"Command text: {context.Message.Content} |> Error: {Result.ErrorReason}");
+            }
+        }
+
+        private async Task UpdateActivity()
+        {
+            string activity = "";
+            using (var s = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Activity.txt"), FileMode.Open, FileAccess.Read))
+            {
+                using (var r = new StreamReader(s))
+                {
+                    activity = r.ReadToEnd();
+                }
+            }
+            string version = "";
+            using (var s = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Version.txt"), FileMode.Open, FileAccess.Read))
+            {
+                using (var r = new StreamReader(s))
+                {
+                    version = r.ReadToEnd();
+                }
+            }
+
+            activity = activity.Replace("{serverCount}", Client.Guilds.Count.ToString());
+            await Client.SetGameAsync($"{activity} {version}");
+        }
+
+        private async Task LogEmbed(SocketMessage msg, ulong logChannelID, DiscordSocketClient client)
+        {
+            Embed[] embeds = msg.Embeds.ToArray();
+            var channel = client.GetChannel(logChannelID) as IMessageChannel;
+            foreach (Embed e in embeds)
+            {
+
+                await channel.SendMessageAsync($"From: {msg.Author} in {msg.Channel} - ", false, e);
+            }
+        }
+
+        private async Task LogAttachment(SocketMessage msg, ulong logChannelID, DiscordSocketClient client)
+        {
+            Attachment[] attached = msg.Attachments.ToArray();
+            var channel = client.GetChannel(logChannelID) as IMessageChannel;
+            string[] imgFileTypes = { ".jpg", ".jpeg", ".gif", ".png"};
+            string path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug\netcoreapp2.1", @"Images");
+
+            foreach (Attachment a in attached)
+            {
+                string file = path + "\\" + a.Filename;
+                string ext = Path.GetExtension(a.Url);
+                if (imgFileTypes.Contains(ext.ToLower()))
+                {
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    using (var c = new WebClient())
+                    {
+                        if (File.Exists(file))
+                        {
+                            File.Delete(file);
+                        }
+                        c.DownloadFile(a.Url, file);
+                        while (c.IsBusy) {}
+
+                    }
+                }
+                if (msg.Content == "")
+                    await channel.SendFileAsync(file, $"From: {msg.Author.Mention} in {MentionUtils.MentionChannel(msg.Channel.Id)}");
+                else
+                    await channel.SendFileAsync(file, $"From: {msg.Author.Mention} in {MentionUtils.MentionChannel(msg.Channel.Id)}\n Included message: {msg.Content}");
+                File.Delete(file);
             }
         }
     }
